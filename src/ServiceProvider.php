cc61929c86182e1540d105a39fb3c52504c18a01
nullthoughts <jani@nullincorporated.com>
2019-10-05 16:59:32 -0400

    Code cleanup

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -25,43 +25,5 @@ class ServiceProvider extends BaseServiceProvider
         Builder::macro('whereLatest', function ($column, $value) {
             return $this->latestRelation()->where($column, $value);
         });
-
-        // Builder::macro('latestRelation', function () 
-        // {
-        //     if(! $where = $this->wheres[0]['second'] ?? null) {
-        //         throw new \InvalidArgumentException('Second join parameter is not specified. The latestRelation() method should only be called from within a whereHas() on a hasMany relationship.');
-        //      }
-
-        //     list($table, $parentRelatedColumn) = explode('.', $where);
-
-        //     return $this->where($table . '.id', function ($sub) use ($where, $table, $parentRelatedColumn) {
-        //         $sub->select('id')
-        //             ->from($table . ' AS sub')
-        //             ->whereColumn('sub.' . $parentRelatedColumn, $where)
-        //             ->latest()
-        //             ->take(1);
-        //     });
-        // });
-
-        // Builder::macro('whereLatest', function ($column, $value) 
-        // {
-        //     return $this->latestRelation()->where($column, $value);
-        // });
-
-        // Builder::macro('latestRelationTwo', function () {
-        //     if(! $where = $this->wheres[0] ?? null) {
-        //         throw new \InvalidArgumentException('Second join parameter is not specified. The latestRelation() method should only be called from within a whereHas() on a hasMany relationship.');
-        //     }
-
-        //     return $this->where('id', function ($sub) use ($where) {
-        //             $sub->from($this->from)
-        //             ->selectRaw('max(id)')
-        //             ->whereRaw($where['first']  . ' = ' . $where['second']);
-        //         });
-        // });
-
-        // Builder::macro('whereLatestTwo', function ($column, $value) {
-        //     return $this->latestRelationTwo()->where($column, $value);
-        // });
     }
 }
